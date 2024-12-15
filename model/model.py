d51fc97450df8cf751d264afd238969888affbfd
0xEnsar <ensaryozgat@gmail.com>
2024-12-16 01:35:53 +0300

    Refactor training method to include both Naive Bayes and Logistic Regression classifiers

--- model/model.py
+++ model/model.py
@@ -17,11 +17,29 @@ class MovieReviewSentimentAnalysis:
         """
         self.dataset = pd.read_csv(dataset_path)
 
-    def fit_naive_bayes(self, x_train, y_train):
+    def train(self, x_train, y_train):
+        """
+        Train the sentiment analysis model
+        """
+        self.__preprocess()
+        self.__fit_naive_bayes(x_train, y_train)
+        self.__fit_logistic_regression(x_train, y_train)
+
+    def __fit_naive_bayes(self, x_train, y_train):
         """
         Train Naive Bayes classifier
         """
         self.nb_classifier = MultinomialNB()
         self.nb_classifier.fit(x_train, y_train)
+
+    def __fit_logistic_regression(self, x_train, y_train):
+        """
+        Train Logistic Regression classifier
+        """
+        self.lr_classifier = LogisticRegression(max_iter=1000)
+        self.lr_classifier.fit(self.X_train, self.y_train)
+    
+    
+    
     
     
